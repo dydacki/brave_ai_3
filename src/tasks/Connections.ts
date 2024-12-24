85d1070de4770d8c0dde60f1ede27b5da740f606
Wojciech Dydacki <dydacki.wojciech@gmail.com>
2024-12-25 00:15:36 +0100

    chore: upgrade bun and minor refactoring to Connections.ts

--- src/tasks/Connections.ts
+++ src/tasks/Connections.ts
@@ -24,62 +24,63 @@ export class Connections extends Task {
 
   async perform(): Promise<void> {
     try {
-      let users = await this.getDatabaseContent<User>('select * from users');
-      let connections = await this.getDatabaseContent<Connection>('select * from connections');
-      console.log(connections);
-
-      await this.neo4jClient.createVectorIndex('user_embedding_index', 'User', 'embedding', 3072);
-      await this.neo4jClient.waitForIndexToBeOnline('user_embedding_index');
-
-      for (const user of users) {
-        await this.neo4jClient.addNode('User', user);
-      }
-
-      for (const connection of connections) {
-        const fromUser = await this.neo4jClient.findNodeByProperty('User', 'id', connection.user1_id);
-        const toUser = await this.neo4jClient.findNodeByProperty('User', 'id', connection.user2_id);
-
-        if (fromUser && toUser) {
-          await this.neo4jClient.connectNodes(fromUser.id, toUser.id, 'KNOWS');
-          console.log(
-            `Connected ${fromUser.id} (${fromUser.properties.username}) to ${toUser.id} (${toUser.properties.username})`,
-          );
-        } else {
-          console.log(`User not found: ${connection.user1_id} or ${connection.user2_id}`);
-        }
-      }
-
+      const [users, connections] = await this.getDatabaseUsers();
+      await this.setupGraphDatabase(users, connections);
       const path = await this.findShortestPath('Rafał', 'Barbara');
-      console.log(path);
-
-      const json = this.createJson(path.join(', '), 'connections');
-      const taskResponse = await this.webClient.post<Json<string>, JsonResponse>('/report', json);
-      console.log(taskResponse);
+      await this.sendResponse(path);
     } catch (error) {
       console.error(error);
     }
   }
 
-  async getDatabaseContent<T>(query: string): Promise<T[]> {
+  async getDatabaseUsers(): Promise<[User[], Connection[]]> {
+    const users = await this.getDatabaseContent<User>('SELECT * FROM users');
+    const connections = await this.getDatabaseContent<Connection>('SELECT * FROM connections');
+    return [users, connections];
+  }
+
+  private async setupGraphDatabase(users: User[], connections: Connection[]): Promise<void> {
+    await this.neo4jClient.createVectorIndex('user_embedding_index', 'User', 'embedding', 3072);
+    await this.neo4jClient.waitForIndexToBeOnline('user_embedding_index');
+
+    for (const user of users) {
+      await this.neo4jClient.addNode('User', user);
+    }
+
+    for (const connection of connections) {
+      const fromUser = await this.neo4jClient.findNodeByProperty('User', 'id', connection.user1_id);
+      const toUser = await this.neo4jClient.findNodeByProperty('User', 'id', connection.user2_id);
+
+      if (fromUser && toUser) {
+        await this.neo4jClient.connectNodes(fromUser.id, toUser.id, 'KNOWS');
+        console.log(
+          `Connected ${fromUser.id} (${fromUser.properties.username}) to ${toUser.id} (${toUser.properties.username})`,
+        );
+      }
+    }
+  }
+
+  private async getDatabaseContent<T>(query: string): Promise<T[]> {
     const restResponse = await this.getQueryResult(query);
     const response = restResponse as Response<T>;
     return response.reply;
   }
 
-  async getQueryResult(query: string): Promise<any> {
+  private async sendResponse(path: string[]): Promise<void> {
+    console.log('Found path:', path);
+    const json = this.createJson(path.join(', '), 'connections');
+    const taskResponse = await this.webClient.post<Json<string>, JsonResponse>('/report', json);
+    console.log(taskResponse);
+  }
+
+  private async getQueryResult(query: string): Promise<any> {
     const url = '/apidb';
     const payload: Query = {task: 'database', apikey: this.apiKey, query};
     const response = await this.webClient.post<Query, any>(url, payload);
     return response;
   }
 
-  async sendResponse(result: string[]): Promise<void> {
-    const json = this.createJson(result, 'connections');
-    const taskResponse = await this.webClient.post<Json<any>, JsonResponse>('/report', json);
-    console.log(taskResponse);
-  }
-
-  async findShortestPath(fromUsername: string, toUsername: string): Promise<string[]> {
+  private async findShortestPath(fromUsername: string, toUsername: string): Promise<string[]> {
     const result = await this.neo4jClient.runQuery(
       `
       MATCH (start:User {username: $fromUsername}),
